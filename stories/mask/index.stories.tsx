d3740c431f7549d40f674f55e0fccc5418634ccc
cenyu <kcenyu10@126.com>
2023-08-16 14:17:39 +0800

    feat: publish all components

--- stories/mask/index.stories.tsx
+++ stories/mask/index.stories.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Meta } from '@storybook/react';
 
 import Mask from '@/mask';
+import Button from '@/button';
+
+import DemoWrap from '../../demos/demo-wrap';
+import DemoBlock from '../../demos/demo-block';
 
 const MaskStory: Meta = {
   title: '反馈/Mask 遮罩层',
@@ -9,13 +13,25 @@ const MaskStory: Meta = {
 };
 
 export const Basic = () => {
-  const [visible, setVisible] = React.useState<boolean>(false);
+  const [visible1, setVisible1] = React.useState<boolean>(false);
+  const [visible2, setVisible2] = React.useState<boolean>(false);
 
   return (
-    <div>
-      <button onClick={() => setVisible(true)}>显示遮罩层</button>
-      <Mask visible={visible} onMaskClick={() => setVisible(false)} />
-    </div>
+    <DemoWrap>
+      <DemoBlock title="基本用法">
+        <Button onClick={() => setVisible1(true)}>显示遮罩</Button>
+        <Mask visible={visible1} onMaskClick={() => setVisible1(false)} />
+      </DemoBlock>
+
+      <DemoBlock title="自定义颜色">
+        <Button onClick={() => setVisible2(true)}>自定义颜色遮罩</Button>
+        <Mask
+          visible={visible2}
+          style={{ '--background': 'rgba(0, 0, 0, .2)' }}
+          onMaskClick={() => setVisible2(false)}
+        />
+      </DemoBlock>
+    </DemoWrap>
   );
 };
 
